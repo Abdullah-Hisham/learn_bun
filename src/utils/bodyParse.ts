5fe56a361f4be27c81f1f7d8cae091289e322a91
Abdullah_Hisham <abdulahhisham15@gmail.com>
2026-04-02 14:02:18 +0200

    1.0.1

--- src/utils/bodyParse.ts
+++
@@ -1,9 +0,0 @@
-// utils/bodyParser.ts
-export async function parseJSON(req: Request) {
-    try {
-    const text = await req.text();       // اقرأ الجسم كنص
-    return text ? JSON.parse(text) : {}; // لو فاضي رجع object فاضي
-    } catch (err) {
-    throw new Error("Invalid JSON");
-    }
-}
